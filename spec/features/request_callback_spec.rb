08970d80b2bf403f58d8cf8ed6de1c3bce75ab18
Ashish Sehra <asehra@gmail.com>
2018-10-11 14:09:06 +0100

    Add all fields

--- spec/features/request_callback_spec.rb
+++ spec/features/request_callback_spec.rb
@@ -4,7 +4,10 @@ RSpec.feature "Request a callback", :type => :feature do
   scenario "User successfully requests a callback" do
     visit "/callback/new"
 
-    fill_in "Name", :with => "John Doe"
+    fill_in "Name", with: "John Doe"
+    fill_in "Business name", with: 'Prospective Ltd'
+    fill_in "Email", with: 'john@prospectiveltd.com'
+    fill_in "Telephone number", with: '02085555555'
     click_button "Request Callback"
 
     expect(page).to have_text("Thanks for the request. We will get back to you within next 3 working days.")
